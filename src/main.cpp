0bc3773ec6f5c8451c77f9ce76fb53e4960b44f0
Akshay <akshaycm@hotmail.com>
2018-03-21 10:27:46 +0300

    premine changes

--- src/main.cpp
+++ src/main.cpp
@@ -1741,7 +1741,7 @@ NOTE:   unlike bitcoin we are using PREVIOUS block height here,
 */
 CAmount GetBlockSubsidy(int nPrevBits, int nPrevHeight, const Consensus::Params& consensusParams, bool fSuperblockPartOnly)
 {
-    if (nPrevHeight == 0) {
+    if (nPrevHeight == 1) {
         return 1510900 * COIN;
     }
     else{
