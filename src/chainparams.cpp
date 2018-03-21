e68ea7bfd8e67ffb76bfafb956baeb5aea1c3e64
Akshay <akshaycm@hotmail.com>
2018-03-21 12:07:27 +0300

    more mining changes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -140,9 +140,9 @@ public:
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
-        fMiningRequiresPeers = true;
+        fMiningRequiresPeers = false;
         fDefaultConsistencyChecks = false;
-        fRequireStandard = true;
+        fRequireStandard = false;
         fMineBlocksOnDemand = true;
         fTestnetToBeDeprecatedFieldRPC = false;
 
